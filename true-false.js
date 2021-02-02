0a42176bff4e772b6a0332c49811827885e3197b
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-02-02 19:23:25 +0600

    True vs False

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// False symbol..
+// 0
+// ""
+// undefined
+// null
+// NuN
+// false
+
+// True symbol..
+// "0", " ", []
+
+let name = {};
+console.log(name);
+if(name){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}

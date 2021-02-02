0ac3ed031091b43e276a1cf998b9a67d061f6b5f
Mahfuz Islam <rjmahfuztech@gmail.com>
2021-02-02 19:57:28 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,29 @@
+// Undefined find way...
+
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+// }
+// const result = add(10, 80);
+// console.log(result);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
+const result = add(10, 80);
+console.log(result);
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+const result = add(20);
+console.log(result);
+
+const identity = {name: "Mahfuz", phone: 9348549438}
+console.log(identity.age);
+
+let fun = undefined;
+console.log(fun);
